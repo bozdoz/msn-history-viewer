38b197ad56f9e98e22c576a948f2198620c2d9db
bozdoz <howaboutben@gmail.com>
2021-01-07 18:02:07 -0400

    initial sentry

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,22 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// adds Sentry for error tracking
+Sentry.init({
+	dsn:
+		"https://1b8869eff143481592033b9e7f72b0be@o69899.ingest.sentry.io/5583213",
+	autoSessionTracking: true,
+	integrations: [new Integrations.BrowserTracing()],
 
+	// We recommend adjusting this value in production, or using tracesSampler
+	// for finer control
+	tracesSampleRate: 1.0,
+});
+
+// Google Analytics for usage stats
 window.addEventListener("hashchange", () => {
 	const { hash } = window.location;
 
@@ -11,3 +24,6 @@ window.addEventListener("hashchange", () => {
 		window.gtag("event", "hash-change", { hash });
 	}
 });
+
+// Render the app
+ReactDOM.render(<App />, document.getElementById("root"));
