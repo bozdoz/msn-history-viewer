cdc7f54cefd27a33a95ccdd6a21cec258474d001
bozdoz <howaboutben@gmail.com>
2021-01-08 10:54:32 -0400

    removes unused package

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -14,9 +13,6 @@ export default () => ({
 	plugins: [
 		nodeResolve({
 			extensions: [".js", ".jsx"],
-		}),
-		replace({
-			"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
 		}),
 		commonjs(),
 		babel({
